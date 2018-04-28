672251296394b08ef5fcd8de24ff420c708f7f97
Sam Szreter <s.szreter@gmail.com>
2018-04-28 13:47:56 +0300

    Add webpack builds

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    entry: './src/index.js',
+    module: {
+        rules: [
+            { test: /\.js$/, exclude: /node_modules/ },
+            { test: /\.css$/, use: [ 'style-loader', 'css-loader' ] },
+        ],
+    },
+    output: {
+        path: path.resolve(__dirname, 'bin'),
+        filename: 'index_bundle.js',
+    },
+    devtool: 'source-map',
+    plugins: [
+        new CleanWebpackPlugin(["bin"]),
+        new HtmlWebpackPlugin(),
+        new CopyWebpackPlugin([
+            { from: 'src/data.json', to: 'data.json' },
+            { from: 'src/img/', to: 'img/' },
+        ]),
+    ],
+}
