5ed1dea77caefcbf5cb35de6172e672799dc3bd4
Sam Szreter <s.szreter@gmail.com>
2018-06-22 17:34:20 +0300

    Update img url

--- src/picture.ts
+++ src/picture.ts
@@ -2,7 +2,8 @@ import * as m from 'mithril';
 
 const SPRITE_COLUMNS: number = 15;
 const IMAGE_SIZE: number = 150;
-const SPRITE_URL = 'https://s3.eu-west-2.amazonaws.com/pokemon-sprite-sheets/sunmoon_sprites.png';
+const IMAGE_SET: string = 'official';
+const SPRITE_URL = `https://s3.eu-west-2.amazonaws.com/pokemon-sprite-sheets/${IMAGE_SET}.png`;
 
 const Picture: m.Component<{
     index: number;
